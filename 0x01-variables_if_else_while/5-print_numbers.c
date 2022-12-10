28708bf4ef2d6c93870c080fa67b99903ae107b1
Uninvincible21 <hanafantu256@gmail.com>
2022-12-10 10:46:58 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print all single digit numbers of base 10 starting from 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
