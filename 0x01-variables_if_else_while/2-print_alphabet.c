3f5e2eb3b3d833a5c2d8494b803c3ee99260066b
Uninvincible21 <hanafantu256@gmail.com>
2022-12-10 10:05:21 -0800

    lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alpha in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
