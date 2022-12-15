1660544bd6081f9c7c84692467bf01942d41ecf1
Uninvincible21 <hanafantu256@gmail.com>
2022-12-15 11:57:08 -0800

    task zero

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,18 @@
 
 /**
  * _isupper - uppercase letters
- * @c: char to check
- *
+ * @c: input character
  * Return: 0 or 1
  */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
