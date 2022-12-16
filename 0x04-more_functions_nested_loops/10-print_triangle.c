fbf8e5eab9a44e557f945a146ded3b05e0d2492d
Uninvincible21 <hanafantu256@gmail.com>
2022-12-16 14:43:40 -0800

    task ten

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle
+ * @size: triangle size
+ *
+ * Return: nothing on success
+ */
+
+void print_triangle(int size)
+{
+	int start = 0, end = size, count;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (count = 0; count < end; count++)
+		{
+			while (start < end)
+			{
+				if ((start + count + 1) >= end)
+					_putchar('#');
+				else
+					_putchar(' ');
+				start++;
+			}
+			start = 0;
+			_putchar('\n');
+		}
+	}
+}
