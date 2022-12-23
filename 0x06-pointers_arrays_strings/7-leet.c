7d2a24c75f5ab469da63e1dcc9af5c19f29144a9
Uninvincible21 <hanafantu256@gmail.com>
2022-12-22 23:26:24 -0800

    task seven

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -16,10 +16,9 @@ char *leet(char *str)
 		for (index2 = 0; index2 <= 7; index2++)
 		{
 			if (str[index1] == leet[index2] ||
-			str[index1] - 32 == leet[index2])
+			 str[index1] - 32 == leet[index2])
 				str[index1] = index2 + '0';
 		}
-		index1++;
 	}
 	return (str);
 }
